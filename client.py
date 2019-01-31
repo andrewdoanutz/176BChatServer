b77512ff824c7d8732e2516c6a57d59cc09e2f77
andrewdoanutz <andrewdoanutz@gmail.com>
2019-01-31 14:59:08 -0800

    send file working in group chat

--- client.py
+++ client.py
@@ -10,19 +10,58 @@ IP_address = str(sys.argv[1])
 Port = int(sys.argv[2]) 
 server.connect((IP_address, Port)) 
   
+
+def getFile(conn):
+    with open('received_file', 'wb') as f:
+        print ('file opened')
+        while True:
+            print('receiving data...')
+            data = conn.recv(1024)
+            print('data=%s', (data))
+            if not data:
+                break
+            # write data to a file
+            f.write(data)
+
+    f.close()
+    print('Successfully get the file')
+
+
+def sendFile(conn,filename):
+   
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('Sent ',repr(l))
+       l = f.read(1024)
+    f.close()
+
+    print('Type done sending to finish sending')
+
+
+
+
 while True: 
 
     sockets_list = [sys.stdin, server] 
     read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
-  
+    
     for socks in read_sockets: 
         if socks == server: 
-            message = socks.recv(2048) 
+            message = socks.recv(2048)
             print (message)
         else: 
             message = sys.stdin.readline() 
-            server.send(message) 
-            sys.stdout.write("<You>") 
-            sys.stdout.write(message) 
-            sys.stdout.flush() 
+            if message[0:9]=='send file':
+                server.send(message)
+                print("Enter filename")
+                filename=sys.stdin.readline()
+                filename=filename[:-1]
+                sendFile(server,filename)
+            else:
+                server.send(message) 
+                sys.stdout.write("<You>") 
+                sys.stdout.write(message) 
+                sys.stdout.flush() 
 server.close() 
--- server.py
+++ server.py
@@ -16,18 +16,51 @@ server.bind((IP_address, Port))
 server.listen(100) 
   
 list_of_clients = [] 
-  
+
+def getFile(conn):
+    with open('received_file', 'wb') as f:
+        print ('file opened')
+        while True:
+            print('receiving data...')
+            data = conn.recv(1024)
+            if data[0:12]=="done sending":
+                break
+            print(data[0:6])
+            print('data=%s', (data))
+            
+            # write data to a file
+            f.write(data)
+
+    f.close()
+    print('Successfully get the file')
+
+
+def sendFile(conn):
+    filename='mytext.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('Sent ',repr(l))
+       l = f.read(1024)
+    f.close()
+
+    print('Done sending')
+
+
 def clientthread(conn, addr): 
     conn.send("Welcome to this chatroom!") 
-  
+    
     while True: 
             try: 
                 message = conn.recv(2048) 
-                if message: 
+                if message[0:9] == "send file": 
+                    broadcastFile("received_file",conn)
+                    getFile(conn)
+                elif message:
                     print ("<" + addr[0] + "> " + message)
                     message_to_send = "<" + addr[0] + "> " + message 
                     broadcast(message_to_send, conn) 
-  
                 else: 
                     remove(conn) 
   
@@ -39,6 +72,15 @@ def broadcast(message, connection):
         if clients!=connection: 
             try: 
                 clients.send(message) 
+            except: 
+                clients.close() 
+                remove(clients) 
+
+def broadcastFile(filename,conn):
+    for clients in list_of_clients: 
+        if clients!=conn: 
+            try: 
+                clients.send(filename)
             except: 
                 clients.close() 
                 remove(clients) 
