1b840d37307f4d7bb9f2549ce24706343e8fe605
Richard Boone <rboone@csil-14.cs.ucsb.edu>
2019-02-23 17:21:09 -0800

    working on file transfer with encryption

--- client.py
+++ client.py
@@ -1,17 +1,17 @@
-import socket 
-import select 
-import sys 
+import socket
+import select
+import sys
 from Crypto.Cipher import AES
-  
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-if len(sys.argv) != 4: 
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 4:
     print ("Correct usage: script, IP address, port number")
-    exit() 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.connect((IP_address, Port)) 
-  
-  
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+server.connect((IP_address, Port))
+
+
 keyfile = str(sys.argv[3])
 key = ""
 try:
@@ -24,15 +24,14 @@ except:
 	file = open(keyfile, "w")
 	file.write(key)
 	file.close()
-	
+
 def encrypt_message(message):
 	#modify to always work even with non-16 length stuff
 	message = message + (16 - (len(message) % 16)) * chr(16 - len(message) % 16)
-	print(len(message))
 	encryptor = AES.new(key)
 	text = encryptor.encrypt(message)
 	return text
-	
+
 def decrypt_message(text):
 	decryptor = AES.new(key)
 	message = decryptor.decrypt(text)
@@ -41,9 +40,9 @@ def decrypt_message(text):
 
 
 def getFile(conn):
-    
+
     filename=conn.read(1024)+'2'
-    
+
     #filename="filelul.txt"
     with open(filename, 'wb') as f:
         while True:
@@ -55,43 +54,44 @@ def getFile(conn):
 
 
 def sendFile(conn,filename):
-    conn.send(filename)
+    conn.send(encrypt_message(filename))
     f = open(filename,'rb')
     l = f.read(1024)
     while (l):
-       conn.send(l)
+       conn.send(encrypt_message(l))
        print("Sent: %r" % repr(l))
        l = f.read(1024)
     f.close()
-    
-    
 
 
 
-while True: 
 
-    sockets_list = [sys.stdin, server] 
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
-    
-    for socks in read_sockets: 
-        if socks == server: 
+
+while True:
+
+    sockets_list = [sys.stdin, server]
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+
+    for socks in read_sockets:
+        if socks == server:
             message = socks.recv(2048)
             # print(message)
             message = decrypt_message(message)
             print (message)
-        else: 
-            message = sys.stdin.readline() 
+        else:
+            message = sys.stdin.readline()
             if message[0:9]=='send file':
+                message = encrypt_message(message)
                 server.send(message)
                 print("Enter filename")
                 filename=sys.stdin.readline()
                 filename=filename[:-1]
                 sendFile(server,filename)
-                server.send("done sending")
+                server.send(encrypt_message("done sending"))
             else:
-                sys.stdout.write("<You>") 
-                sys.stdout.write(message) 
+                sys.stdout.write("<You>")
+                sys.stdout.write(message)
                 message = encrypt_message(message)
-                server.send(message) 
-                sys.stdout.flush() 
-server.close() 
+                server.send(message)
+                sys.stdout.flush()
+server.close()
--- server.py
+++ server.py
@@ -1,19 +1,19 @@
-import socket 
-import select 
-import sys 
-import thread 
+import socket
+import select
+import sys
+import thread
 import os
 from Crypto.Cipher import AES
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
- 
-if len(sys.argv) != 4: 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+if len(sys.argv) != 4:
     print ("Correct usage: script, IP address, port number, keyfile")
-    exit() 
- 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
+    exit()
+
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
 keyfile = str(sys.argv[3])
 key = ""
 try:
@@ -26,24 +26,23 @@ except:
 	file = open(keyfile, "w")
 	file.write(key)
 	file.close()
-	
+
 
 intromessage = "Welcome to this chatroom!"
 
 
-server.bind((IP_address, Port)) 
-server.listen(100) 
-  
-list_of_clients = [] 
+server.bind((IP_address, Port))
+server.listen(100)
+
+list_of_clients = []
 
 def encrypt_message(message):
 	#modify to always work even with non-16 length stuff
 	message = message + (16 - (len(message) % 16)) * chr(16 - len(message) % 16)
-	print(len(message))
 	encryptor = AES.new(key)
 	text = encryptor.encrypt(message)
 	return text
-	
+
 def decrypt_message(text):
 	decryptor = AES.new(key)
 	message = decryptor.decrypt(text)
@@ -55,7 +54,7 @@ def getFile(conn,filename):
     with open(filename, 'wb') as f:
         while True:
             print('receiving data...')
-            data = conn.recv(1024)
+            data = decrypt_message(conn.recv(1024))
             if data[0:12]=="done sending":
                 break
             f.write(data)
@@ -63,7 +62,7 @@ def getFile(conn,filename):
 
 def sendFile(conn,filename):
     conn.send(filename)
-    
+
     f = open(filename,'rb')
     l = f.read(1024)
     while (l):
@@ -73,67 +72,67 @@ def sendFile(conn,filename):
     f.close()
 
     conn.send("done sending")
-    
 
 
-def clientthread(conn, addr): 
+
+def clientthread(conn, addr):
     encrypted_intro = encrypt_message(intromessage)
-    conn.send(encrypted_intro) 
-    while True: 
-            try: 
-                message = conn.recv(2048) 
+    conn.send(encrypted_intro)
+    while True:
+            try:
+                message = conn.recv(2048)
                 message = decrypt_message(message)
-                if message[0:9] == "send file": 
+                if message[0:9] == "send file":
                     filename=conn.recv(1024)
                     print("here")
                     getFile(conn,filename)
                     print("got file")
                     #broadcastFile("received_file",conn)
-                    
+
                 elif message:
                     print ("<" + addr[0] + "> " + message)
                     message_to_send = "<" + addr[0] + "> " + message
                     message_to_send = encrypt_message(message_to_send)
-                    broadcast(message_to_send, conn) 
-                else: 
-                    remove(conn) 
-  
-            except: 
+                    broadcast(message_to_send, conn)
+                else:
+                    remove(conn)
+
+            except:
                 continue
-  
-def broadcast(message, connection): 
-    for clients in list_of_clients: 
-        if clients!=connection: 
-            try: 
-                clients.send(message) 
-            except: 
-                clients.close() 
-                remove(clients) 
+
+def broadcast(message, connection):
+    for clients in list_of_clients:
+        if clients!=connection:
+            try:
+                clients.send(message)
+            except:
+                clients.close()
+                remove(clients)
 
 def broadcastFile(filename,conn):
-    for clients in list_of_clients: 
-        if clients!=conn: 
-            try: 
+    for clients in list_of_clients:
+        if clients!=conn:
+            try:
                 sendFile(filename,clients)
-            except: 
-                clients.close() 
-                remove(clients) 
+            except:
+                clients.close()
+                remove(clients)
 
-def remove(connection): 
-    if connection in list_of_clients: 
-        list_of_clients.remove(connection) 
-  
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
+
 # sometext = "hello, I'm a fnonucasfdasfdasd"
 # interim = encrypt_message(sometext)
 # testtext = decrypt_message(interim)
 # print(testtext)
-  
-  
-while True: 
-    conn, addr = server.accept() 
-    list_of_clients.append(conn) 
+
+
+while True:
+    conn, addr = server.accept()
+    list_of_clients.append(conn)
     print (addr[0] + " connected")
-    thread.start_new_thread(clientthread,(conn,addr))     
-  
-conn.close() 
-server.close() 
+    thread.start_new_thread(clientthread,(conn,addr))
+
+conn.close()
+server.close()
